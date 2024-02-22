083932f710d9b25ca60449f4a6791af193c8b474
Anupam Soni <anupamsoni27@gmail.com>
2024-02-22 10:06:35 -0800

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,24 @@
 import streamlit as st
 import pandas as pd
+import main
+import time
+
 df = pd.read_csv("report.csv")
 df["Date"] = pd.to_datetime(df["Date"])
 st.dataframe(df)
+
+stutus = "Relaxed"
+# Function to read and display data from "report.csv"
+def display_data():
+    df = pd.read_csv("report.csv")
+    df["Date"] = pd.to_datetime(df["Date"])
+
+# Create the button and handle its click event
+if st.button("Run Test and Reload Data"):
+
+    # Replace this placeholder with your actual test function
+    main.runme()
+
+    # Reload data after the test function runs
+    display_data()
+
